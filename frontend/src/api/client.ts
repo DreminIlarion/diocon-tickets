ce4315dfdcc8f3624e03d65d894b71c7a5ff2673
DreminIlarion <anasgodvaan@gmail.com>
2026-05-06 15:17:42 +0500

    06.05.2026 Исправил проброс переменных из env для фронтенда

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -29,7 +29,7 @@ import type {
 } from '@/types';
 import apiClient from './apiClient';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Создаем экземпляр axios
 const api = axios.create({
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,9 +8,23 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
+  //Указываем, где искать .env (на уровень выше)
+  envDir: path.resolve(__dirname, ".."),
+
   plugins: [react(), tailwindcss(), viteSingleFile()],
+
+  //Разрешаем обращаться к файлам выше папки frontend
+  server: {
+    fs: {
+      allow: [
+        path.resolve(__dirname, ".."), // разрешаем корень всего проекта
+        path.resolve(__dirname),      
+      ],
+    },
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
